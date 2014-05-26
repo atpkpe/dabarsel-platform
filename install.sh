10f9b483d33f5ed7640cf75297467c7402b17c2f
mikkel <mikkel@adapt.dk>
2014-05-27 00:02:12 +0200

    Need to popd in order to execute devify.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if pushd "htdocs/sites/default" > /dev/null; then
   drush sql-drop -y
   drush site-install atp --site-name="atp" -y --account-name=adaptadmin --account-pass=1a32b3635e28dc5c43d6
   drush vset cron_key "cron-b4d0fb22d1240c65749f"
+
+  # Run development setup tasks.
+  popd > /dev/null && ./devify.sh
 fi
-# Run development setup tasks.
-./devify.sh
