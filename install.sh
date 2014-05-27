fcaffc4cd11f427762149bca6a59fdb97ce6aeff
mikkel <mikkel@adapt.dk>
2014-05-27 23:52:00 +0200

    Adding l10n_update language update

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 if pushd "htdocs/sites/default" > /dev/null; then
+  # Install site.
   drush sql-drop -y
   drush site-install atp --site-name="atp" -y --account-name=adaptadmin --account-pass=1a32b3635e28dc5c43d6
   drush vset cron_key "cron-b4d0fb22d1240c65749f"
+  
+  # Import language(s).
+  if [ -n $(drush pml --type=module --pipe --no-core --status=enabled | grep l10n_update) ]
+    then
+      drush l10n-update 
+    else
+      echo "l10n_update could not be found. Please activate it."
+    fi
 
   # Run development setup tasks.
   echo "Devifying..."
