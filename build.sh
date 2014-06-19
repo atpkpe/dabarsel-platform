c9e284568229a63ef9a69d71220bee28099cd84e
Tom Helmer Hansen <thelmer@thehome.dk>
2014-06-19 08:56:49 +0200

    build script updated with new situs ignore option

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-drush situs-build --root=htdocs --make-file=./platform.make --git-check
+drush situs-build --root=htdocs --make-file=./platform.make --git-check --git-check-ignore-regex=/global/,/contrib/,/libraries/
