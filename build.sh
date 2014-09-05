4d6981a8d7d889be9242d19b7c31b66073fcd4e0
mikkel <mikkel@adapt.dk>
2014-09-05 10:26:22 +0200

    Adding exclude atp sas files regex to situs

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-drush situs-build --root=htdocs --make-file=./platform.make --git-check --git-check-ignore-regex=/global/,/contrib/,/libraries/
+drush situs-build --root=htdocs --make-file=./platform.make --git-check --git-check-ignore-regex=/global/,/contrib/,/libraries/,/modules/custom/atp_sas/test/
 
 cat patches/atp-admin.css >> htdocs/profiles/atp/themes/contrib/adminimal_theme/css/style.css
