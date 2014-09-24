eac6a6ed3983b109bf49c65a7ef5dc7e9c1a02cf
mikkel <mikkel@adapt.dk>
2014-09-24 14:50:54 +0200

    Cleaning up update.sh a bit

--- scripts/tools/update.sh
+++ scripts/tools/update.sh
@@ -4,10 +4,8 @@
 THIS=$(readlink -f $0)
 THIS_DIR=`dirname $THIS`
 DRUPAL_ROOT=$THIS_DIR/../../site/htdocs
-#alias drush="${THIS_DIR}/drush/drush --root=$DRUPAL_ROOT"
-alias drush="drush --root=$DRUPAL_ROOT"
 
 # Run drush cmds
-drush updb -y
-drush fra -y
-drush cc all
+drush --root=$DRUPAL_ROOT updb -y
+drush --root=$DRUPAL_ROOT fra -y
+drush --root=$DRUPAL_ROOT cc all
