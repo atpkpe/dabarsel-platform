491936b5f3b71e1d50efe5603650e09bf9089bc8
atpkpe <atpkpe@users.noreply.github.com>
2014-12-02 10:47:35 +0100

    Update local_setup.sh

--- local_setup.sh
+++ local_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mysql -uroot -proot -h 127.0.0.1 -e "create database atp_local"
+mysql -uroot -proot -h 127.0.0.1 -e "create database dabarsel_local"
 
 if pushd "htdocs/sites/default" > /dev/null; then
   ln -sf ../../../settings/local.settings.php settings.php
