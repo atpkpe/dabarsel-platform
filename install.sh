d6f15b43232fdc235389429fee33af6df0d62589
Kim Petersen <kpe@atp.dk>
2014-12-02 11:07:40 +0100

    Rename admin user

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 if pushd "htdocs/sites/default" > /dev/null; then
   # Install site.
   drush sql-drop -y
-  drush site-install atp --site-name="atp" -y --account-name=adaptadmin --account-pass=1a32b3635e28dc5c43d6
+  drush site-install atp --site-name="atp" -y --account-name=admin --account-pass=1a32b3635e28dc5c43d6
   drush vset cron_key "cron-b4d0fb22d1240c65749f"
   
   # Import language(s).
